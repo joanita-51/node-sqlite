448415409db1af686bbf7e551dd86ae71ea7dbfd
joanita-51 <nakityojoanita51@gmail.com>
2024-01-18 10:36:38 +0300

    db.get() command

--- app.js
+++ app.js
@@ -23,11 +23,25 @@ const db = new sqlite3.Database('./db.sqlite');
 // })
 
 //Retrieving a single row method 1
-db.all('SELECT * FROM AverageTemp', (error, rows)=>{
+// db.all('SELECT * FROM AverageTemp', (error, rows)=>{
+//     if(error){
+//         console.log(error)
+//         return;
+//     }
+//     console.log(rows.find(row=>row.id==1))
+
+// });
+
+// db.run('INSERT INTO AverageTemp(year, temperature) VALUES (2023, 34.6)',(error)=>{
+//     if(error){
+//         console.log(error)
+//     }
+// })
+
+db.get('SELECT * FROM AverageTemp WHERE year = 2023', (error, row)=>{
     if(error){
         console.log(error)
         return;
     }
-    console.log(rows.find(row=>row.id==1))
-
-});
+    console.log(row)
+})
