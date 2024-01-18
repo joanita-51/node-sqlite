e8062b3ec299d682687248cdf9b7dc45e08b13b9
joanita-51 <nakityojoanita51@gmail.com>
2024-01-18 10:27:22 +0300

    db.all() command

--- app.js
+++ app.js
@@ -12,3 +12,22 @@ const db = new sqlite3.Database('./db.sqlite');
 //         console.log(error)
 //     }
 // })
+
+//Retrieving all rows
+// db.all('SELECT * FROM AverageTemp', function(error, rows){
+//     if(error){
+//         console.log(error)
+//         return;
+//     }
+//     console.log(rows)
+// })
+
+//Retrieving a single row method 1
+db.all('SELECT * FROM AverageTemp', (error, rows)=>{
+    if(error){
+        console.log(error)
+        return;
+    }
+    console.log(rows.find(row=>row.id==1))
+
+});
