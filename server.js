a74a2efc0284086e7e2e599be146e659ec8ad3b0
joanita-51 <nakityojoanita51@gmail.com>
2024-01-19 11:47:29 +0300

    creating a new user

--- server.js
+++ server.js
@@ -2,7 +2,11 @@ const express = require('express');
 const app = express();
 // console.log(app);
 const PORT = 8001;
+const md5 = require('md5')
 const db = require('./app.js')
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 // Root endpoint
 app.get("/", function(req, res, next)
@@ -43,4 +47,35 @@ app.get("/api/user/:id", (req, res, next) => {
       });
 });
 
+app.post("/api/user/", (req, res, next) => {
+    var errors=[]
+    if (!req.body.password){
+        errors.push("No password specified");
+    }
+    if (!req.body.email){
+        errors.push("No email specified");
+    }
+    if (errors.length){
+        res.status(400).json({"error":errors.join(",")});
+        return;
+    }
+    var data = {
+        name: req.body.name,
+        email: req.body.email,
+        password : md5(req.body.password)
+    }
+    var sql ='INSERT INTO user (name, email, password) VALUES (?,?,?)'
+    var params =[data.name, data.email, data.password]
+    db.run(sql, params, function (err, result) {
+        if (err){
+            res.status(400).json({"error": err.message})
+            return;
+        }
+        res.json({
+            "message": "success",
+            "data": data,
+            "id" : this.lastID
+        })
+    });
+})
 app.listen(PORT,()=>{console.log(`The app is running on port ${PORT} `)})
